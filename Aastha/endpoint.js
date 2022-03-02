169e82389a80f5fd99e7a7cc9045b83041feef20
Aastha Grover <74087770+aastha29001@users.noreply.github.com>
2022-03-02 13:30:44 +0530

    Add files via upload

---
+++ Aastha/endpoint.js
@@ -0,0 +1,32 @@
+var numericArr = [];
+var alphaArr = [];
+var model = {
+  is_success: true,
+  user_id: "aastha_29092001",
+  email: "aastha1573.cse19@chitkara.edu.in",
+  roll_number: "1910991573",
+  numbers: numericArr,
+  alphabets: alphaArr,
+};
+
+function sorter(arr) {
+  arr.forEach((i) => {
+    if (isNaN(parseInt(i))) alphaArr.push(i);
+    else numericArr.push(i);
+  });
+}
+
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    res.send("Please refer to /bhfl");
+  });
+
+  app.post("/bhfl", (req, res) => {
+    var data = req.body.data;
+
+    sorter(data);
+    res.json(model);
+    numericArr.length=0
+    alphaArr.length=0
+  });
+};
